2ba4692cabba17587184b86e8d820f06fe3767f6
joseronierison <ronierison.silva@gmail.com>
2019-04-24 19:29:11 -0300

    Add sensitive parameter on cluster information

--- environments/staging/common/output.tf
+++ environments/staging/common/output.tf
@@ -1,17 +1,21 @@
 output "client_certificate" {
   value = "${module.gke_cluster.client_certificate}"
+  sensitive = true
 }
 
 output "client_key" {
   value = "${module.gke_cluster.client_key}"
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
   value = "${module.gke_cluster.cluster_ca_certificate}"
+  sensitive = true
 }
 
 output "endpoint" {
   value = "${module.gke_cluster.endpoint}"
+  sensitive = true
 }
 
 output "zone_name" {
--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -1,13 +1,16 @@
 output "client_certificate" {
   value = "${base64decode(google_container_cluster.gke.master_auth.0.client_certificate)}"
+  sensitive = true
 }
 
 output "client_key" {
   value = "${base64decode(google_container_cluster.gke.master_auth.0.client_key)}"
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
   value = "${base64decode(google_container_cluster.gke.master_auth.0.cluster_ca_certificate)}"
+  sensitive = true
 }
 
 output "endpoint" {
